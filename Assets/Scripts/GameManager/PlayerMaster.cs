7ba5aa69ebcc924e9397903ce273a73e62319f4a
DerTyp187 <janis.meister87@gmail.com>
2021-10-13 17:35:59 +0200

    Added Documentation for Join/Leave List

--- Assets/Scripts/GameManager/PlayerMaster.cs
+++ Assets/Scripts/GameManager/PlayerMaster.cs
@@ -4,48 +4,49 @@ using UnityEngine;
 /*
 JOIN
 1. Wenn ein Spieler joined wird sichergestellt, dass er nicht in der Liste ist (fürs error handling).
-2. Dann wird er in die Liste "Players" eingetragen und ein Eintrag in der "health" liste wird mit dem selben index gemacht.
+2. Dann wird er in die Liste "Players" eingetragen.
 
 LEAVE
 1. Index von dem Spieler, in der Liste, wird gesucht
 2. Spieler wird aus der Liste entfernt
-3. health index vom spieler wird auch entfernt
 
 */
 public class PlayerMaster : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> Players = new List<GameObject>();
+    [SerializeField] private List<GameObject> Players = new List<GameObject>(); //Contains All Players which are currently connected/in-game
     [SerializeField] private List<int> Health = new List<int>();
 
     private void Start()
     {
-        Players.AddRange(GameObject.FindGameObjectsWithTag("Player"));
+        Players.AddRange(GameObject.FindGameObjectsWithTag("Player")); //Add All Player-GameObjects into a List
     }
 
-    public void OnPlayerJoin(GameObject player)
+    //Join
+    public void OnPlayerJoin(GameObject player) //When a Player joins
     {
-        Debug.Log("Player joined");
-        if (!Players.Contains(player))
+        Debug.Log("Player joined"); //Give Console Feedback
+        if (!Players.Contains(player)) //If the Player is NOT in the "Players-List" (For Error Handling)
         {
-            Players.Add(player);
-            Debug.Log("Player added to list");
+            Players.Add(player); //Add New Player To List
+            Debug.Log("Player added to list"); //Feedback
         }
         else
         {
-            Debug.LogError("Player already exits in list");
+            Debug.LogError("Player already exits in list"); //Error, because the "new" Player is already in the list -> !critical Anomaly!
         }
     }
 
-    public void OnPlayerLeave(GameObject player)
+    //Leave
+    public void OnPlayerLeave(GameObject player) //When a Player leaves
     {
-        Debug.Log("Player left");
-        if (Players.Contains(player))
+        Debug.Log("Player left");//Give Console Feedback
+        if (Players.Contains(player))//If the Player IS in the "Players-List" (For Error Handling)
         {
-            Players.Remove(player);
+            Players.Remove(player); //Remove the Player from List
         }
         else
         {
-            Debug.LogError("Player not found in Players-list");
+            Debug.LogError("Player not found in Players-list"); //Error, because the Player is NOT in the list -> !critical Anomaly!
         }
     }
 }
