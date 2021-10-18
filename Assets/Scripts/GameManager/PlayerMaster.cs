266f1a0acab1ec867a9f88fd45c38330dd4e5b37
DerTyp187 <janis.meister87@gmail.com>
2021-10-18 08:57:32 +0200

    Update PlayerMaster.cs

--- Assets/Scripts/GameManager/PlayerMaster.cs
+++ Assets/Scripts/GameManager/PlayerMaster.cs
@@ -1,28 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*
-JOIN
-1. Wenn ein Spieler joined wird sichergestellt, dass er nicht in der Liste ist (fürs error handling).
-2. Dann wird er in die Liste "Players" eingetragen.
-3. Player bekommt einen Eintrag in die Health-Liste (Der Schlüssel ist hier, dass der Index bei beiden Listen für den gleichen Spieler stehen)
 
-LEAVE
-1. Index von dem Spieler, in der Liste, wird gesucht
-2. Spieler wird aus der Liste entfernt
-3. Player wird aus der Health liste ausgetragen
-
-
-HEALTH
- - Set Health of a player
- - Subtract Health from a player
- - Add Health to a player
-
-DEAD
- - Player Dies
- - Another Player gets a kill
-
-*/
 public class PlayerMaster : MonoBehaviour
 {
     [Header("PlayerMaster")]
