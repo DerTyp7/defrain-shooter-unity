f2e8de1537ed7edd97de8fead6af12f35128527b
DerTyp187 <janis.meister87@gmail.com>
2021-10-28 08:29:41 +0200

    idk

--- Assets/Scripts/TestOfflineScene/HostBtnScript.cs
+++ Assets/Scripts/TestOfflineScene/HostBtnScript.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class HostBtnScript : MonoBehaviour
 {
+    private GameObject GameManager;
     private void Start()
     {
         gameObject.GetComponent<Button>().onClick.AddListener(HostServer);
+        GameManager = GameObject.Find("GameManager");
     }
 
     public void HostServer()
     {
-        NetworkClient.ConnectHost();
+        GameManager.GetComponent<NetworkManager>().StartHost();
     }
 }
